08108f06e5a3a07035c6e30f43583a043764c9f9
Xhuljan Sadiku <xsadiku@hotmail.com>
2023-02-15 23:12:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+let userChoice
+let computerChoice
+let result
+
+
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice  
+    generateComputerChoice()
+    getResult()
+}))
+
+
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length ) + 1 // or Math.floor(Math.random() * 3 ) +  1 
+    
+    if (randomNumber === 1) {
+        computerChoice = 'Rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'Scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+
+function getResult() {
+    if(computerChoice === userChoice) {
+        result = "I'ts a draw!"
+    }
+    if(computerChoice === 'Rock' &&  userChoice === 'Paper') {
+        result = "You Win!"
+    }
+    if(computerChoice === 'Rock' &&  userChoice === 'Scissors') {
+        result = "You Lose!"
+    }
+    if(computerChoice === 'Paper' &&  userChoice === 'Scissors') {
+        result = "You Win!"
+    }
+    if(computerChoice === 'Paper' &&  userChoice === 'Rock') {
+        result = "You Lose!"
+    }
+    if(computerChoice === 'Scissors' &&  userChoice === 'Rock') {
+        result = "You Win!"
+    }
+    if(computerChoice === 'Scissors' &&  userChoice === 'Paper') {
+        result = "You Lose!"
+    }
+    resultDisplay.innerHTML = result
+}
